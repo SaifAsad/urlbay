a28270a5ca19e4074b17e6f113b8f528f9592a68
saifasad <saif.salah88@yahoo.com>
2015-08-13 13:13:47 +1200

    "implemented bootsrap and shared layout page and view for BrowseURLs controller"

---
+++ DAL/URLdb.cs
@@ -0,0 +1,53 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//used repository pattern with entity framework
+
+namespace DAL
+{
+    class URLdb
+    {
+        private LinkHubDBEntities db;
+
+        public URLdb()
+        {
+            db = new LinkHubDBEntities();
+        }
+
+        IEnumerable<tbl_Url> GetAll()
+        {
+            return db.tbl_Url.ToList();
+        }
+
+        public tbl_Url GetByID(int id)
+        {
+            return db.tbl_Url.Find(id);
+        }
+
+        public void Insert(tbl_Url url)
+        {
+            db.tbl_Url.Add(url);
+            Save();
+        }
+        public void Delete(int id)
+        {
+            tbl_Url url = db.tbl_Url.Find(id);
+            db.tbl_Url.Remove(url);
+            Save();
+        }
+
+        public void Update(tbl_Url url)
+        {
+            db.Entry(url).State = System.Data.Entity.EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}

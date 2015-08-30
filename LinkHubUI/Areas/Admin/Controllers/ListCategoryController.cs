845151e6b12b78e19ccf3b570c671ea7e6466b5d
saifasad <saif.salah88@yahoo.com>
2015-08-30 16:13:20 +1200

    added listusers and listcategories views and controllers

--- LinkHubUI/Areas/Admin/Controllers/ListCategoryController.cs
+++ LinkHubUI/Areas/Admin/Controllers/ListCategoryController.cs
@@ -3,15 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL;
 
 namespace LinkHubUI.Areas.Admin.Controllers
 {
     public class ListCategoryController : Controller
     {
-        // GET: Admin/ListCategory
-        public ActionResult Index()
+        private CategoryBs objBs;
+
+        public ListCategoryController()
         {
-            return View();
+            objBs = new CategoryBs();
         }
-    }
+
+        // GET: Admin/ListCategory
+        public ActionResult Index(String SortOrder, String SortBy, String Page)
+        {
+            ViewBag.SortOrder = SortOrder;
+            ViewBag.SortBy = SortBy;
+
+            var categories = objBs.GetAll().ToList();
+
+            switch (SortBy)
+            {
+                case "CategoryName":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            categories = categories.OrderBy(x => x.CategoryName).ToList();
+                            break;
+
+                        case "Desc":
+                            categories = categories.OrderByDescending(x => x.CategoryName).ToList();
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "CategoryDesc":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            categories = categories.OrderBy(x => x.CategoryDesc).ToList();
+                            break;
+
+                        case "Desc":
+                            categories = categories.OrderByDescending(x => x.CategoryDesc).ToList();
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                default:
+                    categories = categories.OrderBy(x => x.CategoryName).ToList();
+                    break;
+            }
+
+            ViewBag.TotalPages = Math.Ceiling(objBs.GetAll().Count() / 10.0);
+
+            int page = int.Parse(Page == null ? "1" : Page);
+            //page number is needed in order to highlight the page
+            ViewBag.Page = page;
+
+
+            //categories contaiins a list of objects of all the categories in the db, we use the page number to extract display the revelant 
+            //number of pages
+            //if page == 2, 2 - 1 = 1 * 10 = 10, skip the first 10 records and take next 10
+
+            categories.ToList();
+            int elementCount = (page - 1) * 10;
+
+            List<BOL.tbl_Category> returnedCategories = new List<BOL.tbl_Category>(categories.Count());
+            int internalCount = 0;
+
+            for (int i = elementCount; internalCount < 10 && i < categories.Count(); i++, internalCount++)
+            {
+                if (categories.ElementAt(i) != null)
+                    returnedCategories.Add(categories.ElementAt(i));
+            }
+            //urls = urls.Skip((page - 1) * 10).Take(10);
+
+            return View(returnedCategories);
+        }
 }
--- LinkHubUI/Areas/Admin/Controllers/ListUserController.cs
+++ LinkHubUI/Areas/Admin/Controllers/ListUserController.cs
@@ -3,15 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL;
 
 namespace LinkHubUI.Areas.Admin.Controllers
 {
     public class ListUserController : Controller
     {
-        // GET: Admin/ListUser
-        public ActionResult Index()
+        private UserBs objBs;
+
+        public ListUserController()
         {
-            return View();
+            objBs = new UserBs();
+        }
+
+        // GET: Admin/ListUser
+        public ActionResult Index(String SortOrder, String SortBy, String Page)
+        {
+            ViewBag.SortOrder = SortOrder;
+            ViewBag.SortBy = SortBy;
+
+            var users = objBs.GetAll().ToList(); ;
+
+            switch (SortBy)
+            {
+                case "UserEamil":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            users = users.OrderBy(x => x.UserEmail).ToList();
+                            break;
+
+                        case "Desc":
+                            users = users.OrderByDescending(x => x.UserEmail).ToList();
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "Role":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            users = users.OrderBy(x => x.Role).ToList();
+                            break;
+
+                        case "Desc":
+                            users = users.OrderByDescending(x => x.Role).ToList();
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    users = users.OrderBy(x => x.UserEmail).ToList();
+                    break;
+            }
+            ViewBag.TotalPages = Math.Ceiling(objBs.GetAll().Count() / 10.0);
+
+            int page = int.Parse(Page == null ? "1" : Page);
+            //page number is needed in order to highlight the page
+            ViewBag.Page = page;
+
+
+            //urls contaiins a list of objects of all the urls in the db, we use the page number to extract display the revelant 
+            //number of pages
+            //if page == 2, 2 - 1 = 1 * 10 = 10, skip the first 10 records and take next 10
+
+            users.ToList();
+            int elementCount = (page - 1) * 10;
+
+            List<BOL.tbl_User> returnedUsers = new List<BOL.tbl_User>(users.Count());
+            int internalCount = 0;
+
+            for (int i = elementCount; internalCount < 10 && i < users.Count(); i++, internalCount++)
+            {
+                if (users.ElementAt(i) != null)
+                    returnedUsers.Add(users.ElementAt(i));
+            }
+            //urls = urls.Skip((page - 1) * 10).Take(10);
+
+            return View(returnedUsers);
         }
     }
 }
